7ecfbc44fefa3102ea566345ef454f26b19df80f
poojithapalladi <122811760+poojithapalladi@users.noreply.github.com>
2024-10-15 13:40:42 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,121 @@
 import {Route, Switch, Redirect} from 'react-router-dom'
+import {Component} from 'react'
+
 import LoginRoute from './components/LoginRoute'
 import HomeRoute from './components/HomeRoute'
+import TrendingRoute from './components/TrendingRoute'
+import GamingRoute from './components/GamingRoute'
+import SavedVideosRoute from './components/SavedVideosRoute'
 import NotFoundRoute from './components/NotFoundRoute'
+import VideoItemDetailsRoute from './components/VideoItemDetailsRoute'
+import ProtectedRoute from './components/ProtectedRoute'
+
+import ActiveMenuContext from './context/ActiveMenuContext'
+import ThemeContext from './context/ThemeContext'
+import SavedVideosContext from './context/SavedVideosContext'
 
 import './App.css'
 
-const App = () => (
-  <Switch>
-    <Route exact path="/login" component={LoginRoute} />
-    <Route exact path="/" component={HomeRoute} />
-    <Route exact path="/not-found" component={NotFoundRoute} />
-    <Redirect to="/not-found" />
-  </Switch>
-)
+const activeMenuConstants = {
+  initial: 'INITIAL',
+  home: 'HOME',
+  trending: 'TRENDING',
+  gaming: 'GAMING',
+  savedVideos: 'SAVED_VIDEOS',
+}
+
+class App extends Component {
+  state = {
+    isDarkTheme: false,
+    activeMenu: activeMenuConstants.home,
+    savedVideosList: [],
+    save: false,
+  }
+
+  addVideosToSavedVideos = videoDetails => {
+    this.setState(prev => ({
+      savedVideosList: [...prev.savedVideosList, videoDetails],
+    }))
+  }
+
+  deleteVideosFromSavedVideos = videoDetails => {
+    const {savedVideosList} = this.state
+    const updatedList = savedVideosList.filter(
+      each => each.id !== videoDetails.id,
+    )
+    this.setState({savedVideosList: updatedList})
+  }
+
+  updateSaveVideosList = videoDetails => {
+    const {save} = this.state
+    if (save) {
+      this.deleteVideosFromSavedVideos(videoDetails)
+    } else {
+      this.addVideosToSavedVideos(videoDetails)
+    }
+  }
+
+  updateSave = videoDetails => {
+    this.setState(
+      prev => ({save: !prev.save}),
+      this.updateSaveVideosList(videoDetails),
+    )
+  }
+
+  changeTheme = () => {
+    this.setState(prev => ({isDarkTheme: !prev.isDarkTheme}))
+  }
+
+  changeActiveMenu = value => {
+    this.setState({activeMenu: value})
+  }
+
+  render() {
+    const {isDarkTheme, activeMenu, save, savedVideosList} = this.state
+
+    return (
+      <ThemeContext.Provider
+        value={{isDarkTheme, changeTheme: this.changeTheme}}
+      >
+        <SavedVideosContext.Provider
+          value={{
+            save,
+            savedVideosList,
+            addVideosToSavedVideos: this.addVideosToSavedVideos,
+            deleteVideosFromSavedVideos: this.deleteVideosFromSavedVideos,
+            updateSave: this.updateSave,
+          }}
+        >
+          <ActiveMenuContext.Provider
+            value={{activeMenu, changeActiveMenu: this.changeActiveMenu}}
+          >
+            <Switch>
+              <Route exact path="/login" component={LoginRoute} />
+              <ProtectedRoute exact path="/" component={HomeRoute} />
+              <ProtectedRoute
+                exact
+                path="/trending"
+                component={TrendingRoute}
+              />
+              <ProtectedRoute exact path="/gaming" component={GamingRoute} />
+              <ProtectedRoute
+                exact
+                path="/saved-videos"
+                component={SavedVideosRoute}
+              />
+              <ProtectedRoute
+                exact
+                path="/videos/:id"
+                component={VideoItemDetailsRoute}
+              />
+              <Route path="/not-found" component={NotFoundRoute} />
+              <Redirect to="/not-found" />
+            </Switch>
+          </ActiveMenuContext.Provider>
+        </SavedVideosContext.Provider>
+      </ThemeContext.Provider>
+    )
+  }
+}
 
 export default App
